6ad935c31fea89a7bd14875ec26081d65bf7f8e9
Duschmolll <65500704+Duschmolll@users.noreply.github.com>
2024-10-02 10:54:10 +0200

    Game is good normally

--- Greg/mastermind_fromFIles.py
+++ Greg/mastermind_fromFIles.py
@@ -89,11 +89,13 @@ def distance(a: list, b: list) -> float:
 
 
 def getChoixCouleur() -> None:
-    while True:
+    inner_running = True
+    while inner_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print("c'est fini!!!")
                 pygame.quit()
+                inner_running = False
             if event.type == pygame.MOUSEBUTTONUP:
                 positionSouris = pygame.mouse.get_pos()
                 if distance(positionSouris, [75, 80 + 40 * 9]) < 15:
@@ -145,7 +147,8 @@ def afficherResultat(f: pygame.Surface, res, ligne):
 
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 500))
+screen: pygame = pygame.display.set_mode((1000, 750))
+screen.fill(Blanc)
 
 
 def randomSecret() -> list:
@@ -162,4 +165,50 @@ afficherPlateau(screen)
 afficherChoixCouleur(screen)
 afficherSecret(screen, secretCode)
 
-construireProposition(screen, 2)
+
+def checkanswerPlayer():
+    propPlayer = construireProposition(screen, userTry)
+
+    wellPlaced = 0
+    wrongPlaced = 0
+
+    for i in range(5):
+        if secretCode[i] == propPlayer[i]:
+            wellPlaced += 1
+        else:
+            for k in range(i, 5):
+                if secretCode[i] == propPlayer[k]:
+                    wrongPlaced += 1
+
+    resultat = (wellPlaced, wrongPlaced)
+    afficherResultat(screen, resultat, userTry)
+    return wellPlaced
+
+
+secretBroken = False
+gameEnded = False
+userTry = 2
+
+while not secretBroken and not gameEnded:
+    if userTry == 17:
+        gameEnded == True
+    elif checkanswerPlayer() == 5:
+        secretBroken = True
+    else:
+        userTry += 1
+        print(userTry)
+
+if secretBroken == True:
+    print("GG, vous avez trouvé la combinaison en ", userTry - 1, " coups !")
+    # afficherSecret(screen, secretCode)
+elif gameEnded == True:
+    print("Perdu, vous dépassé vos 15 coups")
+    # afficherSecret(screen, secretCode)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("c'est fini!!!")
+            pygame.quit()
+            running = False
