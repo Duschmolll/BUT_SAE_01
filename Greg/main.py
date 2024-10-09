51e431767f8649ad0f4fd9c4b37b77545905623f
Duschmolll <65500704+Duschmolll@users.noreply.github.com>
2024-10-09 09:23:11 +0200

    Commented

--- Greg/main.py
+++ Greg/main.py
@@ -6,6 +6,7 @@ import pygame
 secretCode = []
 
 
+# On utlise un randInt(0,7) 5 fois pour choisir aléatoirement 5 couleurs de la liste TabCouleur.
 def randomSecret() -> list:
 
     for i in range(5):
@@ -13,6 +14,7 @@ def randomSecret() -> list:
     return secretCode
 
 
+# Fonction pour vérifier la proposition du joueurs face au code secret.
 def checkanswerPlayer(propPlayer: tuple, secret: list):
 
     wellPlaced = 0
@@ -20,12 +22,14 @@ def checkanswerPlayer(propPlayer: tuple, secret: list):
     alreadyCheckSecret = []
     alreadyCheckPlayer = []
 
+    # Boucle pour regarder si des couleurs de la prop du Joueurs sont présent et bien placé
     for i in range(5):
         if secret[i] == propPlayer[i]:
             wellPlaced += 1
             alreadyCheckSecret.append(i)
             alreadyCheckPlayer.append(i)
 
+    # Boucle pour regarder si des couleurs de la prop du Joueurs sont présent mais mal placé en faisant attention au doublon.
     for i in range(5):
         for k in range(0, 5):
             if (
@@ -40,8 +44,9 @@ def checkanswerPlayer(propPlayer: tuple, secret: list):
     return (wellPlaced, wrongPlaced)
 
 
+# Fonction principale du programme.
 def main():
-
+    # Mise en route de pygame.
     pygame.init()
     screen: pygame = pygame.display.set_mode((1000, 750))
     screen.fill(mm.Blanc)
@@ -49,14 +54,17 @@ def main():
     # secretCode = randomSecret()
     secretCode = [mm.Noir, mm.Blanc, mm.Vert, mm.Vert, mm.Rouge]
 
+    # Mise en place du GUI en utilisant les fonction de mm.py
     mm.afficherPlateau(screen)
     mm.afficherChoixCouleur(screen)
     mm.afficherSecret(screen, secretCode)
 
+    # Variable
     secretBroken = False
     gameEnded = False
     userTry = 2
 
+    # Boucle pour continuer à demander une proposition si ne joueurs est à moins de 15 essaie et que le code n'as pas été trouvé.
     while not secretBroken and not gameEnded:
         check = checkanswerPlayer(mm.construireProposition(screen, userTry), secretCode)
         mm.afficherResultat(screen, check, userTry)
@@ -68,6 +76,7 @@ def main():
             userTry += 1
             print(userTry)
 
+    # Savoir quoi afficher lors de la sortie de la boucle précédente. Perdu ou Gagner.
     if secretBroken == True:
         print("GG, vous avez trouvé la combinaison en ", userTry - 1, " coups !")
         # afficherSecret(screen, secretCode)
@@ -75,6 +84,7 @@ def main():
         print("Perdu, vous dépassé vos 15 coups")
         # afficherSecret(screen, secretCode)
 
+    # Laisser tourner Pygame tant que le joueurs n'as pas décidé de quitter le jeu.
     running = True
     while running:
         for event in pygame.event.get():
--- Greg/tester.py
+++ Greg/tester.py
@@ -1,8 +1,10 @@
 import main
 import mm
 import math
+import random
 
 
+# Test de la fonction de comparaison du code secret et de la proposition du joueur.
 def testerChoixPlayer():
     secretCode = [mm.Noir, mm.Blanc, mm.Vert, mm.Vert, mm.Rouge]
     propPlayer1 = [mm.Noir, mm.Blanc, mm.Vert, mm.Vert, mm.Rouge]
@@ -35,33 +37,48 @@ def testerChoixPlayer():
         print("Test 4 KAPUT: ", result, "| Expected : (1, 0)")
 
 
+# Test de la fonction distance.
 def testDistance():
     if mm.distance([0, 0], [1, 1]) == math.sqrt(2):
         print("Test MM 01 OK")
     else:
         print("Test MM 01 KAPUT")
     if mm.distance([1, 1], [1, 1]) == math.sqrt(0):
-        print("Test MM 01 OK")
+        print("Test MM 02 OK")
     else:
-        print("Test MM 01 KAPUT")
+        print("Test MM 02 KAPUT")
     if mm.distance([0, 0], [-1, -1]) == math.sqrt(2):
-        print("Test MM 01 OK")
+        print("Test MM 03 OK")
     else:
-        print("Test MM 01 KAPUT")
+        print("Test MM 03 KAPUT")
     if mm.distance([1, 1], [0, 0]) == math.sqrt(2):
-        print("Test MM 01 OK")
+        print("Test MM 04 OK")
     else:
-        print("Test MM 01 KAPUT")
+        print("Test MM 04 KAPUT")
     if mm.distance([0, 0], [0, 0]) == math.sqrt(0):
-        print("Test MM 01 OK")
+        print("Test MM 05 OK")
     else:
-        print("Test MM 01 KAPUT")
+        print("Test MM 05 KAPUT")
     if mm.distance([-1, -1], [-1, -1]) == 0:
-        print("Test MM 01 OK")
+        print("Test MM 06 OK")
     else:
-        print("Test MM 01 KAPUT")
+        print("Test MM 06 KAPUT")
+
+
+# Test que la randomisation du code secret prends bien toutes les couleurs possible.
+def testRandom():
+    rangeofRand = []
+    for i in range(1000):
+        rand = random.randint(0, 7)
+        if rand not in rangeofRand:
+            rangeofRand.append(rand)
+    rangeofRand.sort()
+    if rangeofRand == [0, 1, 2, 3, 4, 5, 6, 7]:
+        print("Test Random Range OK")
+    else:
+        print("Random Range KAPUT: ", rangeofRand, " | Expected: [0,1,2,3,4,5,6]")
 
 
 testerChoixPlayer()
-# testDistance()
-# print(mm.distance([-1, -1], [-1, -1]))
+testRandom()
+testDistance()
