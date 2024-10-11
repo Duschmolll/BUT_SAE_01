12b3f064ba2ae9e860435fd262619e0e38e8889a
Matt <mattieu.marecaille59149@gmail.com>
2024-10-11 23:07:23 +0200

    fonction check  good
    fonction check refaite avec mon pote ,main réarranger rapidement ,fonction test modifier pour qu'elle fonctionne
    (Bisou grégou)<3

--- Mattieu/main2.py
+++ Mattieu/main2.py
@@ -1,14 +1,14 @@
 from mm import*
 # décla variables globale
 secretCode = []
-secretCode = [Noir, Blanc, Vert, Vert, Rouge]
+#secretCode = [Noir, Blanc, Vert, Vert, Rouge]
+testprop=[Vert,Vert,Vert,Blanc,Vert]
 essaiJoueur:int=2
 
 
 
 
 def randomSecret() -> list:#Fonction permettant de créer la combinaison secrétes de pion a trouver par le joueur
-
     for i in range(5):
         secretCode.append(TabCouleur[random.randint(0, 7)])
     return secretCode
@@ -17,13 +17,34 @@ def randomSecret() -> list:#Fonction permettant de créer la combinaison secrét
 def check(laProposition:list)->None:#Fonction vérifiant  le nb de pion bien placé ou non entre la combinaison créer par le joueur et le code Secret
     wellPlaced=0
     wrongPlace=0
-    print(len(secretCode))
-    for i in range (5):
-        if secretCode[i]==laProposition[i]:
-            wellPlaced+=1
-            for k in range (i+1,5):
-                if secretCode[i]==laProposition[k]:
+    secretCodeCopy=secretCode.copy()
+    propCopy=laProposition.copy()
+
+    print(len(secretCodeCopy))
+    i=0
+    while i< len(secretCodeCopy):        
+        if secretCodeCopy[i]==propCopy[i]:
+            secretCodeCopy.pop(i)
+            propCopy.pop(i)
+            wellPlaced+=1             
+        else:
+            i+=1
+    i=0
+    while i<len(secretCodeCopy):
+        k=0
+        while k<len(secretCodeCopy):
+            if secretCodeCopy[i]==propCopy[k] :
+                    secretCodeCopy.pop(i)
+                    propCopy.pop(k)
+                    i-=1
                     wrongPlace+=1
+                    k=len(secretCodeCopy)
+            else:
+                k+=1
+        i+=1
+        
+                
+
     return (wellPlaced,wrongPlace)
 
 def main():
@@ -31,7 +52,7 @@ def main():
     screen:pygame = pygame.display.set_mode((1000, 750))
     screen.fill(Blanc)
     game_is_running = True
-    #secretCode = randomSecret()
+    secretCode = randomSecret()
     print(secretCode)
     afficherPlateau(screen)
     afficherChoixCouleur(screen)
@@ -40,18 +61,17 @@ def main():
     while game_is_running :
         for event in pygame.event.get():
             if event.type==pygame.QUIT :
-                running=False
+                game_is_running=False
                 pygame.quit()
-            prop = construireProposition(screen,essaiJoueur)
-            print(check(prop))
-            while prop !=secretCode and essaiJoueur<5:
-                    afficherResultat(screen,check(prop),essaiJoueur)
-                    essaiJoueur+=1
-                    prop = construireProposition(screen,essaiJoueur)        
-            if prop == secretCode:
-                afficherResultat(screen,check(prop),essaiJoueur)
-            
-    
-main()
+        prop = construireProposition(screen,essaiJoueur)
+        print(check(prop))
+        afficherResultat(screen,check(prop),essaiJoueur)
+        essaiJoueur+=1
+        pygame.display.update()       
+        if prop == secretCode:
+            afficherResultat(screen,check(prop),essaiJoueur)
+
+if __name__=="__main__":
+    main()
 
 
